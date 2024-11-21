32a888c45058f8e9f77a701e05e5040845b1d63b
Prateek Mehta <pmehta@starlinewindows.com>
2024-11-21 13:55:39 -0800

    adds extension

--- wwwroot/viewer.js
+++ wwwroot/viewer.js
@@ -1,5 +1,9 @@
 ﻿/// import * as Autodesk from "@types/forge-viewer";
 
+import './extensions/LoggerExtension.js';
+import './extensions/SummaryExtension.js';
+import './extensions/HistogramExtension.js';
+
 async function getAccessToken(callback) {
     try {
         const resp = await fetch('/api/auth/token');
@@ -18,7 +22,7 @@ export function initViewer(container) {
     return new Promise(function (resolve, reject) {
         Autodesk.Viewing.Initializer({ env: 'AutodeskProduction', getAccessToken }, function () {
             const config = {
-                extensions: ['Autodesk.DocumentBrowser']
+                extensions: ['Autodesk.DocumentBrowser','LoggerExtension','SummaryExtension','HistogramExtension']
             };
             const viewer = new Autodesk.Viewing.GuiViewer3D(container, config);
             viewer.start();
